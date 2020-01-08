96604710a14acdae1f56bc682e030f60307d6da9
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-08 00:32:58 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,7 +43,18 @@ def naming_system
   # HoH all the way to Species!
   hash = {
    label: "Kingdom",
-   sub_category: { label: "Phylum", sub_category: { label: "Class", sub_category: {}} }
+   sub_category: {
+     label: "Phylum", sub_category: {
+       label: "Class", sub_category: {
+         label: "Order", sub_category: {
+           label: "Family", sub_category: {
+             label: "Genus",sub_category: {
+  label: "Species",
+  sub_category: nil
+}
+  }
+         }
+       }} }
   }
   return hash
 end
